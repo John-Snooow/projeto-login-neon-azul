360228aa570617ad5a4ce21eeba065f0efade699
John Frank <139596702+John-Snooow@users.noreply.github.com>
2024-08-19 21:56:06 -0300

    fazendo comentários no js

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
+// Seleciona o elemento com a classe "wrapper" no documento HTML
 const wrapper = document.querySelector('.wrapper');
+
+// Seleciona o elemento com a classe "register-link" no documento HTML
 const registerLink = document.querySelector('.register-link');
+
+// Seleciona o elemento com a classe "login-link" no documento HTML
 const loginLink = document.querySelector('.login-link');
 
+// Adiciona um evento de clique ao elemento "registerLink"
 registerLink.onclick = () => {
-    wrapper.classList.add('active');
+  // Adiciona a classe "active" ao elemento "wrapper"
+  wrapper.classList.add('active');
 }
+
+// Adiciona um evento de clique ao elemento "loginLink"
 loginLink.onclick = () => {
-    wrapper.classList.remove('active');
+  // Remove a classe "active" do elemento "wrapper"
+  wrapper.classList.remove('active');
 }
